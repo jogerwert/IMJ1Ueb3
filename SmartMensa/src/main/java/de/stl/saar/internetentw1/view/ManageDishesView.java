4532748b9d2cd6e2ea934d003ab2771abbe88e90
jogerwert <jogerwert@web.de>
2019-04-12 04:07:13 +0200

    unnötige imports in manageDishesView entfernt

--- SmartMensa/src/main/java/de/stl/saar/internetentw1/view/ManageDishesView.java
+++ SmartMensa/src/main/java/de/stl/saar/internetentw1/view/ManageDishesView.java
@@ -3,12 +3,9 @@ package de.stl.saar.internetentw1.view;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
-import javax.faces.event.ValueChangeEvent;
 import javax.faces.model.DataModel;
 import javax.faces.model.ListDataModel;
 
